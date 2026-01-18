9ba77102a2e427e2b103e8b59df2e2dcd0a779ee
Kazi Tanim Shadid <150577958+tanim-shadid@users.noreply.github.com>
2026-01-18 22:49:30 +0600

    Add files via upload

---
+++ 11_Container With Most Water.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0,r=height.size()-1;
+        int ans=0;
+        while(l<r)
+        {
+            int cnt=(r-l)*min(height[l],height[r]);
+            ans=max(ans,cnt);
+            if(height[l]<height[r])l++;
+            else r--;
+        }
+        return ans;
+    }
+};
