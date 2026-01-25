9361a00917f0bf8464c40e436e34f1e086442aa8
Kazi Tanim Shadid <150577958+tanim-shadid@users.noreply.github.com>
2026-01-25 21:05:04 +0600

    Add files via upload

---
+++ 572_Subtree of Another Tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(subRoot==NULL)return true;
+        if(root==NULL)return false;
+        if(identical(root,subRoot))return true;
+        return isSubtree(root->left,subRoot) ||
+        isSubtree(root->right,subRoot);
+
+
+    }
+    private:
+    bool identical(TreeNode *root,TreeNode *subRoot)
+    {
+        if(root==NULL && subRoot==NULL)return true;
+        if(root!=NULL && subRoot!=NULL &&
+         root->val==subRoot->val)
+        {
+            return identical(root->left,subRoot->left) &&
+              identical(root->right,subRoot->right);
+
+        }
+        return false;
+    }
+};
