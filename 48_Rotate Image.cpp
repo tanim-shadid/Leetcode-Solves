fcf633c23047c76f5514d76cc947f7fca4be3b57
Kazi Tanim Shadid <150577958+tanim-shadid@users.noreply.github.com>
2026-03-09 15:51:05 +0600

    Add files via upload

---
+++ 48_Rotate Image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int row=matrix.size();
+        int col=matrix.size();
+      for(int i=0;i<row;i++)
+        {
+            for(int j=i+1;j<col;j++)
+            {
+                 swap(matrix[i][j],matrix[j][i]);
+
+            }
+        }
+         for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col/2;j++)
+            {
+                swap(matrix[i][j],matrix[i][col-j-1]);
+            }
+        }
+        return;
+    }
+};
