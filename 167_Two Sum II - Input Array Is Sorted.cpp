af4294b14221a8fee9d90f31f6f727fcb3053c6c
Kazi Tanim Shadid <150577958+tanim-shadid@users.noreply.github.com>
2026-04-01 23:27:20 +0600

    Add files via upload

---
+++ 167_Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& v, int t) {
+        vector<int>ans;
+        int l=0,r=v.size()-1;
+        while(l<r)
+        {
+            if(v[l]+v[r]==t)
+            {
+                ans.push_back(l+1);
+                ans.push_back(r+1);
+                break;
+            }
+            else if(v[l]+v[r]<t)
+            {
+               l++;
+            }
+            else if(v[l]+v[r]>t)
+            {
+                r--;
+            }
+        }
+        return ans;
+
+    }
+};
