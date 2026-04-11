c785cfbda8a97c8b3ac41f70566e974e5ef9e2ad
Kazi Tanim Shadid <150577958+tanim-shadid@users.noreply.github.com>
2026-04-11 22:58:35 +0600

    Add files via upload

---
+++ 137_Single Number II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+       map<long long,long long>m;
+       //int ans=0;
+       for(int i=0;i<nums.size();i++)
+       {
+         m[nums[i]]++;
+       }
+       for(auto it:m)
+       {
+        if(it.second==1)
+        {
+            return it.first;
+        }
+       }
+       return -1;
+    }
+};
