247b12c954222f2c5d8806e61f36cf80b5c13d93
Kazi Tanim Shadid <150577958+tanim-shadid@users.noreply.github.com>
2026-08-23 16:37:02 +0600

    Add files via upload

---
+++ 416_Partition Equal Subset Sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if(sum&1)return false;
+        int half=sum/2;
+        vector<vector<bool>>dp(n+1,vector<bool>(half+1,false));
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=true;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=half;j++)
+            {
+                if(nums[i-1]<=j)
+                {
+                    dp[i][j]=dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][half];
+    }
+};
